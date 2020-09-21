3a56d08e778ce3c0f6ca85b9d84a533d72807ef8
ariffirmansyah <ariffirmansyah.begin@gmail.com>
2020-09-21 14:38:24 +0700

    up

--- app/Http/Libraries/Auth.php
+++ app/Http/Libraries/Auth.php
@@ -46,7 +46,7 @@ class Auth {
             if (isset($data['email']) && !empty($data['email'])) {
                 $user_exist = DB::table('tbl_users')->where('is_active', 1)->where('email', $data['email'])->first(); //$Tbl_users->find('first', array('fields' => 'all', 'table_name' => 'tbl_users', 'conditions' => array('where' => array('a.is_active' => '= "1"', 'a.email' => '="' . $data['email'] . '"'))));
             } else {
-                $user_exist = DB::table('tbl_users')->where('is_active', 1)->where('username', $data['userid'])->orWhere('code', $data['userid'])->first(); //$Tbl_users->find('first', array('fields' => 'all', 'table_name' => 'tbl_users', 'conditions' => array('where' => array('a.is_active' => '= "1"', 'a.username' => '="' . $data['userid'] . '"'), 'or' => array('a.code' => '="'.$data['userid']. '"'))));
+                $user_exist = DB::table('tbl_users')->where('is_active', 1)->orWhere('username', $data['userid'])->orWhere('code', $data['userid'])->first(); //$Tbl_users->find('first', array('fields' => 'all', 'table_name' => 'tbl_users', 'conditions' => array('where' => array('a.is_active' => '= "1"', 'a.username' => '="' . $data['userid'] . '"'), 'or' => array('a.code' => '="'.$data['userid']. '"'))));
             }
             debug($user_exist);
             $res = Auth::verify_hash($data['password'], $user_exist->password);
