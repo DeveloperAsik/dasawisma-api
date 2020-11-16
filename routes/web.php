b304ac37a7ffeb9280bcfe38a79b71e5e85441b4
ariffirmansyah <ariffirmansyah.begin@gmail.com>
2020-11-16 22:51:08 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -60,7 +60,7 @@ Route::get('/fetch/icon', 'Api\Master\IconController@get_list')->name('get-list-
 //fetching report
 Route::get('/fetch/report-incidents', 'Api\Reports\IncidentsController@get_list')->name('get-list-reports-incidents');
 Route::get('/fetch/report-incidents-logs', 'Api\Reports\IncidentsController@get_log_list')->name('get-reports-incidents-logs');
-Route::get('/find/report-incidents', 'Api\Reports\IncidentsController@find')->name('find-reports-incidents');
+//Route::get('/find/report-incidents', 'Api\Reports\IncidentsController@find')->name('find-reports-incidents');
 Route::post('/transmit/report-incidents', 'Api\Reports\IncidentsController@insert')->name('transmit-report-incidents');
 
 Route::get('/latest/activity', 'Api\Settings\UserController@get_latest_activity')->name('get-latest-activity');
